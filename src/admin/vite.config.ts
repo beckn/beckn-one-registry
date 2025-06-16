26c1e921f53dc430fe3ce31dbfcea98bb82b468e
shreyvishal <shreya.vishal@eminds.ai>
2025-06-16 17:41:22 +0530

    Added: Vite config

---
+++ src/admin/vite.config.ts
@@ -0,0 +1,21 @@
+import { server } from "@strapi/strapi/admin/test";
+import { mergeConfig, type UserConfig } from "vite";
+
+export default (config: UserConfig) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    resolve: {
+      alias: {
+        "@": "/src"
+      }
+    },
+    server: {
+      allowedHosts: [
+        "localhost",
+        "127.0.0.1",
+        "0.0.0.0",
+        "dedi-registry.becknprotocol.io"
+      ]
+    }
+  });
+};
